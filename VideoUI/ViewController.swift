2632fa987ea9c89c83361c55bc238fb145ca148e
Rokas <rookas.rudzenskas@gmail.com>
2023-06-22 11:51:18 +0300

    INITIAL

---
+++ VideoUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoUI
+//
+//  Created by Rookas Rudzianskas on 6/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
