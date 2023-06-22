8589ffee7a73fbbc3ba5a4553842117879db4021
Rokas <rookas.rudzenskas@gmail.com>
2023-06-22 14:02:15 +0300

    ✨ feat(project): add HeroHeaderUIView.swift file to Views group

---
+++ VideoUI/Views/HeroHeaderUIView.swift
@@ -0,0 +1,33 @@
+//
+//  HeroHeaderUIView.swift
+//  VideoUI
+//
+//  Created by Rookas Rudzianskas on 6/22/23.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+}
